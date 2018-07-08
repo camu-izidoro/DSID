1a373e3c4b2a650039da1961139b94ae29af00b1
camu-izidoro <camila.izidoro@usp.br>
2018-07-08 20:12:32 -0300

    vqmvqm

--- client.py
+++ client.py
@@ -4,10 +4,20 @@ from datetime import datetime
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 
-s = xmlrpc.client.ServerProxy('http://192.168.0.17:8000')
-ini = time.time() 
+##função inutil na execução
+inid = time.time() 
+sd= xmlrpc.client.ServerProxy('http://192.168.0.17:8000')
 try:
-  z =  s.long_1_param(123223423)
+    sd.teste()
+except xmlrpc.client.Fault as err:
+    h=1
+fimd = time.time()
+print("DESCARTAR Função teste PARAMETRO E RETURN : ", fimd-inid)
+
+ini = time.time() 
+s4= xmlrpc.client.ServerProxy('http://192.168.0.17:8000')
+try:
+  z =  s4.long_1_param(123223423)
 except xmlrpc.client.Fault as err:
     print("A fault occurred")
     print("Fault code: %d" % err.faultCode)
@@ -15,7 +25,17 @@ except xmlrpc.client.Fault as err:
 fim = time.time()
 print("Função C 1 PARAMETRO E RETURN : ", fim-ini)
 
+ini3 = time.time() 
+s1 = xmlrpc.client.ServerProxy('http://192.168.0.17:8000')
+try:
+   s1.c_void()
+except xmlrpc.client.Fault as err:
+    h=1
+fim3 = time.time()
+print("Função c void", fim3- ini3)
+
 ini2 = time.time() 
+s = xmlrpc.client.ServerProxy('http://192.168.0.17:8000')
 try:
    x= s.long_8_param(123223423,123223423,123223423,123223423,123223423,123223423,123223423,123223423)
 except xmlrpc.client.Fault as err:
@@ -25,17 +45,18 @@ except xmlrpc.client.Fault as err:
 fim2 = time.time()
 print("Função C 8 PARAMETROS E 1 RETURN ", fim2- ini2)
 
-ini3 = time.time() 
-try:
-   s.teste()
-except xmlrpc.client.Fault as err:
-    h=1
-fim3 = time.time()
-print("Função c void", fim3- ini3)
 
+class Node:
+    def __init__(self):
+        self.__atributo = 1
+
+No = Node()
+No.__atributo=1
 ini4 = time.time() 
+
+s2= xmlrpc.client.ServerProxy('http://192.168.0.17:8000')
 try:
-  x= s.retornaNo('abcd')
+  x= s2.retornaNo(No)
 except xmlrpc.client.Fault as err:
     h=1
 fim4 = time.time()
@@ -43,12 +64,55 @@ print("Função c tipo complexo", fim4- ini4)
 
 
 ini5 = time.time() 
+s5 = xmlrpc.client.ServerProxy('http://192.168.0.17:8000')
 try:
-  x= s.uma_string('aadorq3942894jtuehfsurhwuer')
+  x= s5.uma_string('aadorq3942894jtuehfsurhwuer')
 except xmlrpc.client.Fault as err:
+    print("excecao")
     h=1
 fim5 = time.time()
-print("Função c string", fim5 - ini5)
+print("Função c 1 string retorna 1 string", fim5 - ini5)
+
+ini6 = time.time() 
+s6 = xmlrpc.client.ServerProxy('http://192.168.0.17:8000')
+try:
+  x= s6.oito_string('aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer')
+except xmlrpc.client.Fault as err:
+    print("excecao")
+    h=1
+fim6 = time.time()
+print("Função c 8 string retorna 1 string", fim6 - ini6)
+
+ini7 = time.time() 
+s7 = xmlrpc.client.ServerProxy('http://192.168.0.17:8000')
+try:
+  x= s7.dezeseis_string('aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer')
+except xmlrpc.client.Fault as err:
+    print("excecao")
+    h=1
+fim7 = time.time()
+print("Função c 16 string retorna 1 string", fim7 - ini7)
+
+ini8 = time.time() 
+s8 = xmlrpc.client.ServerProxy('http://192.168.0.17:8000')
+try:
+  x= s8.trintaeduas_string('aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer','aadorq3942894jtuehfsurhwuer')
+except xmlrpc.client.Fault as err:
+    print("excecao")
+    h=1
+fim8 = time.time()
+print("Função c 32 string retorna 1 string", fim8 - ini8)
+
+arrayTeste = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+ini9 = time.time() 
+s9 = xmlrpc.client.ServerProxy('http://192.168.0.17:8000')
+try:
+  x= s9.vetor(arrayTeste)
+except xmlrpc.client.Fault as err:
+    print("excecao")
+    h=1
+fim9 = time.time()
+print("Função c vetor retorna vetor", fim9 - ini9)
 
 num = input("Digite x para fechar:")
 print(num)
--- rpc.py
+++ rpc.py
@@ -1,13 +1,18 @@
 from xmlrpc.server import SimpleXMLRPCServer
-
+from array import array
 
 import datetime
-def teste():
+def c_void():
     i=0
     for x in range(0, 1000):
         for z in range(0,1000):
             i = i+x+z
-            
+
+def teste():
+    i=0
+    for x in range(0, 1000):
+        for z in range(0,1000):
+            i = i+x+z            
 
 def long_8_param(a,b,c,d,e,f,g,h):
     i=0
@@ -30,30 +35,61 @@ def uma_string(a):
             i = i+x+z
     return a
 
-class Node:
-    def __init__(self):
-        self.__atributo = None
-        self.__filhos = {}
-    
-def retornaNo(x):
+def oito_string(a,b,c,d,e,f,g,h):
     i=0
     for x in range(0, 1000):
         for z in range(0,1000):
             i = i+x+z
-    Root = Node()
-    return Root
+    return a
+
+def dezeseis_string(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p):
+    i=0
+    for x in range(0, 1000):
+        for z in range(0,1000):
+            i = i+x+z
+    return a
+
+def trintaeduas_string(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,x,y,z,w,aa,ab,ac,ad,ae,af):
+    i=0
+    for x in range(0, 1000):
+        for z in range(0,1000):
+            i = i+x+z
+    return a
+
+class Node:
+    def __init__(self):
+        self.__atributo = 1
+    
+def retornaNo(obj):
+    i=0
+    for x in range(0, 1000):
+        for z in range(0,1000):
+            i = i+x+z
+    return obj
+
+def vetor(a):
+    i=0
+    for x in range(0, 1000):
+        for z in range(0,1000):
+            i = i+x+z
+    return a
 
 
 with SimpleXMLRPCServer(("192.168.0.17",8000)) as server:
     server.register_function(pow)
     server.register_function(teste, 'teste')
+    server.register_function(c_void, 'c_void')
     server.register_function(long_8_param, 'long_8_param')
     server.register_function(long_1_param, 'long_1_param')
     server.register_function(uma_string, 'uma_string')
+    server.register_function(oito_string, 'oito_string')
+    server.register_function(dezeseis_string, 'dezeseis_string')
+    server.register_function(trintaeduas_string, 'trintaeduas_string')
     server.register_function(retornaNo, 'retornaNo')
+    server.register_function(vetor,'vetor')
 
     server.register_multicall_functions()
-    print('Serving XML-RPC on localhost port 7070')
+    print('Serving XML-RPC on localhost port 8000')
     try:
         server.serve_forever()
     except KeyboardInterrupt:
